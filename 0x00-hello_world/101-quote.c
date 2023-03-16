e764e1e6df23070846bdd98a31df4419f5e2b221
medboua83 <bouazza.medyt2@gmail.com>
2023-03-16 05:43:17 -0700

    tt

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-   * main - prints to the standard using fprintf
+   * main - prints  using fprintf
    *
    * Return: one
    */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (1);
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
 }
