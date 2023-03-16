6fdf27d0b5608da48a1b53af8b7337d8ccddedd5
medboua83 <bouazza.medyt2@gmail.com>
2023-03-16 05:23:36 -0700

    tt

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+   * main - print to screen using printf
+   *
+   * Return: zero
+   */
+int main(void)
+{
+char message[] = "with proper grammar, but the outcome is a piece of art,";
+printf(message);
+return (0);
+}
