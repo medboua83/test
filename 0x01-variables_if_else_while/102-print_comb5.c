0778066e45dad827523822c7e06addbbdafa879e
medboua83 <bouazza.medyt2@gmail.com>
2023-03-17 15:31:22 -0700

    default

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - Software is eating the World
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int i;
+	int y;
+	int x;
+	int z;
+
+	for (i = 48 ; i <= 56 ; i++)
+	{
+		for (y = 48 ; y <= 57 ; y++)
+		{
+			for (x = 48 ; x <= 57 ; x++)
+			{
+				for (z = 48 ; z <= 57 ; z++)
+				{
+					if ((((i - 48) * 10) + (y - 48)) < (((x - 48) * 10) + (z - 48)))
+					{
+						putchar(i);
+						putchar(y);
+						putchar(' ');
+						putchar(x);
+						putchar(z);
+						if (!(i == 57 && y == 56 && x == 57 && z == 57))
+						{
+                                                	putchar(',');
+                                                	putchar(' ');
+                                        	}
+					
+					}
+				}
+			}
+
+		}
+	}
+	putchar(10);
+	return (0);
+}
