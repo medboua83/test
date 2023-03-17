14b4210c36ca284dc9ec46f76da2fd8d394775fe
medboua83 <bouazza.medyt2@gmail.com>
2023-03-17 08:14:21 -0700

    default

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - using putchar to print alphabets
+ * main - Hexadecimal
  *
  * Return: zero
  */
@@ -13,7 +13,7 @@ int main(void)
 	{
 		putchar(i);
 	}
-	for (i = 97 ; i<= 102 ; i++)
+	for (i = 97 ; i <= 102 ; i++)
 	{
 		putchar(i);
 	}
