8adec70b5f4c9bb0f168d3fc99d9bb65d04da9e0
medboua83 <bouazza.medyt2@gmail.com>
2023-03-17 08:19:31 -0700

    default

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,8 +13,12 @@ int main(void)
 	for (i = 48 ; i <= 57 ; i++)
 	{
 		putchar(i);
-		putchar(',');
-		putchar(' ');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+
+		}
 	}
 	putchar(10);
 	return (0);
