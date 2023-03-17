5bda15308649ac44792bff8c826a6228d2fce070
medboua83 <bouazza.medyt2@gmail.com>
2023-03-17 10:51:54 -0700

    default

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main -Inventing is a combination of brains and materials.
+ * The more brains you use, the less material you need
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int i;
+	int y;
+
+	for (i = 48 ; i <= 56 ; i++)
+	{
+		for (y = 48 ; y <= 57 ; y++)
+		{
+			for (x = 48 ; x<= 57 ; x++)
+			{
+				if (i < y && y < x && i < x)
+				{
+					putchar(i);
+                                        putchar(y);
+					putchar(x);
+					if (!(i == 55 && y == 56 && x == 57))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+
+		}
+	}
+	putchar(10);
+	return (0);
+}
