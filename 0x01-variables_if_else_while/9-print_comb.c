6317c964c3ef2822128dd7d91bd0f18f7420843c
medboua83 <bouazza.medyt2@gmail.com>
2023-03-17 08:46:47 -0700

    default

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Patience, persistence and perspiration 
+ * main - Patience, persistence and perspiration
  * make an unbeatable combination for success
  *
  * Return: zero
