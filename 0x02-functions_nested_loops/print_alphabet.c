929d31c97d55c5266cdcb33a34133c6c1a8cf770
medboua83 <bouazza.medyt2@gmail.com>
2023-03-21 06:51:57 -0700

    default

---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Patience, persistence and perspiration
+ * make an unbeatable combination for success
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	void print_alphabet(void)
+	{
+		int i;
+
+		for (i = 97 ; i <= 122 ; i++)
+		{
+			_puthcar(i);
+		}
+		putchar('\n')
+	}
+	return (0);
+}
