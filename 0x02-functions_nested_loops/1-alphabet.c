f3be91835ab38a213e4826fbd51c7108c6a16eb6
medboua83 <bouazza.medyt2@gmail.com>
2023-03-21 07:10:05 -0700

    default

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,18 +5,19 @@
  *
  * Return: Always 0.
  */
+void print_alphabet(void)
+{
+	int i;
+
+        for (i = 97 ; i <= 122 ; i++)
+        {
+		_putchar(i);
+        }
+	_putchar('\n');
+}
+
 int main(void)
 {
-	void print_alphabet(void)
-        {
-                int i;
-
-                for (i = 97 ; i <= 122 ; i++)
-                {
-                        _putchar(i);
-                }
-                _putchar('\n');
-	}
 
 	print_alphabet();
 	return (0);
