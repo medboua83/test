041cb1dad8daef3a416c132cb892f6e7cd10fc54
medboua83 <bouazza.medyt2@gmail.com>
2023-03-21 05:53:30 -0700

    default

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,18 +8,6 @@
  */
 int main(void)
 {
-	int i;
-
-	for (i = 48 ; i <= 57 ; i++)
-	{
-		putchar(i);
-		if (i < 57)
-		{
-			putchar(',');
-			putchar(' ');
-
-		}
-	}
-	putchar(10);
+	prinft("_putchar");
 	return (0);
 }
