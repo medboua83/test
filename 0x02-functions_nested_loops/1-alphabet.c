b6ee212e80122853614e5eaeb8d716a54fd3a741
medboua83 <bouazza.medyt2@gmail.com>
2023-03-21 13:47:53 -0700

    default

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,17 @@
-#include "main.h"
 #include "print_alphabet.h"
-#include "stdio.h"
-
+#include "main.h"
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+        int i;
+
+        for (i = 97 ; i <= 122 ; i++)
+        {
+                _putchar(i);
+        }
+        _putchar('\n');
 }
