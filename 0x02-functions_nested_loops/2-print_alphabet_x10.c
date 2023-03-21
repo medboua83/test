4484cf90562c92b4ae6a4e2e1a22dbd4553dab80
medboua83 <bouazza.medyt2@gmail.com>
2023-03-21 14:13:08 -0700

    default

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,11 @@
 
 void print_alphabet_x10(void)
 {
-	int i,x;
+	int i;
 
 	for(i = 0; i <= 10 ; i++)
 	{
-		for (i = 97 ; i <= 122 ; i++)
-		{
-			_putchar(i);
-		}
-		_putchar('\n');
+		print_alphabet();
 	}
+	
 }
