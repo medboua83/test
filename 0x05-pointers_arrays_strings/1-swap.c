0d77c92c34ce07a9bc2d37e86fd47796091f7694
medboua83 <bouazza.medyt2@gmail.com>
2023-03-27 13:21:15 -0700

    default

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98  - updates the value it points to to 98.
+ * @n: PONTER OF  integer
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	*a = temp;
+	*a = *n;
+	*n = temp;
+}
