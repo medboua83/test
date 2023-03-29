97b1b6592050504c6e8e84cd8463aba940ef0c3f
medboua83 <bouazza.medyt2@gmail.com>
2023-03-29 14:37:14 -0700

    default

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,18 +4,19 @@
  * _strncat -  concatenates two strings.
  * @dest: pointer of char
  * @src: pointer of char
+ * @n: number of bytes
  * Return: Returns a pointer to the resulting string @dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	 int dest_len = strlen(dest);
-	 int i;
-	 
-	 for (i = 0; i < n && src[i] != '\0'; i++)
-	 {
-		 dest[dest_len + i] = src[i];
-	 }
-	 dest[dest_len + i] = '\0';
-	 return dest;
+	int dest_len = strlen(dest);
+	int i;
+	
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+	dest[dest_len + i] = '\0';
+	return (dest);
 }
