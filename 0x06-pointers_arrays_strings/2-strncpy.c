773fa76d2f4b6ccdbbfeb752bf9e4ded1a02fa86
medboua83 <bouazza.medyt2@gmail.com>
2023-03-30 09:34:59 -0700

    default

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,12 +9,13 @@
  * Return: Returns a a copy  of @src in  @dest
  */
 
- char *_strncpy(char *dest, const char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 	for ( ; i < n; i++)
 		dest[i] = '\0';
-	return dest;
+	return (dest);
 }
