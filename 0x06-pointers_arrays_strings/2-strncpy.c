0a4830bebf3238a95ec5ed875ea575f01505fcdb
medboua83 <bouazza.medyt2@gmail.com>
2023-03-30 09:29:26 -0700

    default

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncpy  -   copies the string pointed to by src,
+ * including the terminating null byte ('\0')
+ * @dest: pointer of char
+ * @src: pointer of char
+ * @n: number of bytes
+ * Return: Returns a a copy  of @src in  @dest
+ */
+
+ char *_strncpy(char *dest, const char *src, int n)
+{
+	int i;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return dest;
+}
